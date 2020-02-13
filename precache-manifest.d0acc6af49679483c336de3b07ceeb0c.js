70d8f6bc288d9007dda3dbd00883a9ef329fbb73
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2020-02-13 17:09:44 -0300

    Nova build

---
+++ precache-manifest.d0acc6af49679483c336de3b07ceeb0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3266409ff0f40a9205c2340deb102c",
+    "url": "/zhub-teeth-selection-example/index.html"
+  },
+  {
+    "revision": "9b9dcba8960fc2052388",
+    "url": "/zhub-teeth-selection-example/static/css/main.2bb86569.chunk.css"
+  },
+  {
+    "revision": "3c0dc051e067dd5bb764",
+    "url": "/zhub-teeth-selection-example/static/js/2.fde3927e.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/zhub-teeth-selection-example/static/js/2.fde3927e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9dcba8960fc2052388",
+    "url": "/zhub-teeth-selection-example/static/js/main.1c91ee72.chunk.js"
+  },
+  {
+    "revision": "c5054e9c43214d3ce7c3",
+    "url": "/zhub-teeth-selection-example/static/js/runtime-main.c49df47e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b35866eeb911ba44c9a0bb467dcbfbf.js"
+  "/zhub-teeth-selection-example/precache-manifest.d0acc6af49679483c336de3b07ceeb0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zhub-teeth-selection-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
